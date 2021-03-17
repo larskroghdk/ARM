c64d440f0f66f34ace411d7e5e2155b70d254b24
Lars Krogh <larskroghdk@users.noreply.github.com>
2021-03-17 08:39:24 +0100

    Create prep-vm.ps1

---
+++ VM/Scripts/prep-vm.ps1
@@ -0,0 +1,8 @@
+# Install modules to update Windows
+Install-PackageProvider -Name "Nuget" -Force:$true -Confirm:$false
+Install-Module PSWindowsUpdate -Confirm:$false -Force:$true -SkipPublisherCheck:$true -Scope AllUsers
+Get-WUInstall -WindowsUpdate -AcceptAll -IgnoreReboot -Confirm:$false -ErrorAction Stop
+
+# Generalize the VM
+$sysprepPath = $env:windir + '\System32\Sysprep\sysprep.exe'
+Start-Process -FilePath $sysprepPath -ArgumentList ("/oobe /shutdown /generalize /quiet")
